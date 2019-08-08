5dabb9103512049c95001b79a9f7afbf9de84f37
Pedro Gomez <phg1608@gmail.com>
2019-08-08 16:28:39 -0300

    Aula 2

---
+++ Aula2/Exercise2.py
@@ -0,0 +1,11 @@
+# Aula 2 Exercício 2
+# 1.
+print(str(42*60 + 42) + ' seconds')
+
+# 2.
+print(str(10 / 1.61) + ' miles')
+
+# 3.
+print(str(((42*60 + 42) / (10 / 1.61) ) / 60) + ' minutes per mile')
+print(str((10 / 1.61) / (((42*60 + 42) / 60) / 60)) + ' mph')
+
