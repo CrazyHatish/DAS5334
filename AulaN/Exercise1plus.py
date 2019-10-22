6ffa0b1f471404ea37916f6fd3c40ea754061e47
Pedro Gomez <phg1608@gmail.com>
2019-10-22 13:39:25 -0300

    Aula N+1 part 2

--- AulaN/Exercise1plus.py
+++ AulaN/Exercise1plus.py
@@ -1,25 +1,25 @@
-from collections import Iterable
-from operator import add
+from collections.abc import Iterable
+from operator import add, mul
 from math import factorial
 
-def sum_list(l, func, op):
-    s = 0
-    if isinstance(l, Iterable):
+def sum_list(l, func=float, op=add, init=0):
+    s = init
+    if isinstance(l, Iterable) and not (isinstance(l, str) and len(l) == 1):
         for el in l:
-            op(sum_list(el, func, op), s)
+            s = op(s, sum_list(el, func, op, init))
 
         return s
 
     else:
-        return func(float(l))
+        return func(l)
 
 
 def sum_list_str(l):
     string = str(l).replace('[','').replace(']','')
-    print(string)
     num_list = string.split(', ')
     return sum([float(num) for num in num_list])
 
-my_list = [[[1, 2, [[3]]], [3, 7, 2]], [[8], 0], 1]
+my_list = [[[1, 2, [[3]]], [3, 7, 2]], [[8], 2], 1]
 
-print(sum_list(my_list, factorial, add))
+print(sum_list(my_list))
+print(sum_list(my_list, func=lambda x: [x], init=[]))
